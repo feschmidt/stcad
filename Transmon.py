61b18f6ac5811546930eae12784d6e6f92cf37d9
Sarwan Peiter - TNW <srpeiter@tudelft.net>
2017-07-10 02:11:36 +0200

    Added code for Transmon with squidloop, next to put in: array of junctions and code to position elements on chip

--- Transmon.py
+++ Transmon.py
@@ -137,37 +137,142 @@ class singlejuction_transmon(transmon):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 class squidjunction_transmon(transmon):
 	"""
 	This class returns a squid junction Yale Transmon
 	"""
 
-	def __init__(self, dict_pads, dict_squidloop, dict_junction, name):
+	def __init__(self, name, dict_pads, dict_squidloop, dict_junctions):
 
-		super(singlejuction_Transmon, self).__init__()
+		transmon.__init__(self,name, dict_pads, dict_junctions)
 
 		self.dict_pads = dict_pads
-		self.dict_leads = dict_leads
 		self.dict_squidloop = dict_squidloop
-		self.dict_junction = dict_junction
+		self.dict_junctions = dict_junctions
 
-		self.draw_pads()
-		self.draw_squidloop();
-		self.draw_leads()
-		self.draw_junction()
+		overl_junc_lead = 2
+		
+		self.position_offs_junc_y = self.dict_pads['height'] + self.dict_pads['lead_height'] +\
+								 self.dict_squidloop['squid_height'] + self.dict_squidloop['squid_thickness'] +\
+								 - overl_junc_lead
+
+		self.position_offs_junc_x = 0.5*self.dict_squidloop['squid_width'] + 0.5*self.dict_squidloop['squid_thickness'] 
+		
+		
+		self.pad_spacing = 2*self.position_offs_junc_y + 2*(self.dict_junctions['bjunction_height'] +\
+									self.dict_junctions['junction_height']) + dict_junctions['w_dolan_bridge'] +\
+									self.dict_junctions['appr_overlap'] 
+
+
+
+		self.draw_pads()					
+
+		self.draw_junctions()
+		
+		layout = cad.core.Layout('LIBRARY')
+		layout.add(self.cell)
+		layout.save('output.gds')
+		layout.show()
+
+	def draw_pads(self):
+
+		width = self.dict_pads.get('width', 250)
+		height = self.dict_pads.get('height', 600)
+		lead_width = self.dict_pads.get('lead_width',10)
+		lead_height = height +  self.dict_pads.get('lead_height',20)
+		fork_depth = self.dict_pads.get('fork_depth', 1)
+		rounded_edges = self.dict_pads.get('rounded_edges', False)
+		layer = self.dict_pads['layer']
+
+		squid_thickness =  2* self.dict_squidloop.get('squid_thickness',3)
+		squid_width = self.dict_squidloop.get('squid_width', 10)
+		squid_height = self.dict_squidloop.get('squid_height', 10)
+	
+		
+
+		squid_width_wrap = squid_thickness + squid_width
+		squid_height_wrap = 0.5*squid_thickness+ lead_height + squid_height
+		
+		# Now make 2 cells for the upper pad and lower pad
+		# We also divide thcikness by 2, that's the reason for 6 in the
+		# denominator
+		pads = cad.core.Cell("PADS")
+		lower_pad_points = [(-0.5*width,0),
+							(0.5*width,0),
+							(0.5*width,height),
+							(0.5*lead_width,height),
+							(0.5*lead_width,lead_height),
+							(0.5*squid_width_wrap,lead_height),
+							(0.5*squid_width_wrap, squid_height_wrap),
+							(0.5*squid_width_wrap - (1/6.)*squid_thickness,squid_height_wrap),
+							(0.5*squid_width_wrap - (1/6.)*squid_thickness,squid_height_wrap-fork_depth),
+							(0.5*squid_width_wrap - (2/6.)*squid_thickness,squid_height_wrap-fork_depth),
+							(0.5*squid_width_wrap - (2/6.)*squid_thickness,squid_height_wrap),
+							(0.5*squid_width, squid_height_wrap),
+							(0.5*squid_width, squid_height_wrap - squid_height),
+							(-0.5*squid_width, squid_height_wrap - squid_height),
+							(-0.5*squid_width, squid_height_wrap),
+							(-0.5*squid_width - (1/6.)*squid_thickness,squid_height_wrap),
+							(-0.5*squid_width - (1/6.)*squid_thickness,squid_height_wrap-fork_depth),
+							(-0.5*squid_width - (2/6.)*squid_thickness,squid_height_wrap-fork_depth),
+							(-0.5*squid_width - (2/6.)*squid_thickness,squid_height_wrap),
+							(-0.5*squid_width_wrap,squid_height_wrap),
+							(-0.5*squid_width_wrap,lead_height),
+							(-0.5*lead_width,lead_height),
+							(-0.5*lead_width, height),
+							(-0.5*width,height)]
+		
+		lower_pad = cad.core.Boundary(lower_pad_points,
+											layer = layer)
 		
 
 
+		if rounded_edges:
+		
+			corners = collections.OrderedDict()
+			corners['BL0'] = 0
+			corners['BR1'] = 1
+			corners['TR2'] = 2
+			corners['BL3O'] = 3
+			corners['TL4O'] = 4
+			corners['BR5'] = 5
+			corners['TR6'] = 6
+			corners['TL7'] = 7
+			corners['BR8O'] = 8
+			corners['BL9O'] = 9
+			corners['TR10'] = 10
+			corners['TL11'] = 11
+			corners['BR12O'] = 12
+			corners['BL13O'] = 13
+			corners['TR14'] = 14
+			corners['TL15'] = 15
+			corners['BR16O'] = 16
+			corners['BL17O'] = 17
+			corners['TR18'] = 18
+			corners['TL19'] = 19
+			corners['BL20'] = 20
+			corners['TR21O'] = 21
+			corners['BR22O'] = 22
+			corners['TL23'] = 23
+
+			
+			rad_corner = 0.3
+			lower_pad = utilities.make_rounded_edges(lower_pad,
+													rad_corner, 
+													corners)
+
+			
+		upper_pad = cad.utils.translate(cad.utils.reflect(lower_pad,'x'),(0,self.pad_spacing))
+		pad_list = cad.core.Elements([lower_pad,upper_pad])
+		pads.add(pad_list)
+		
+
+		self.cell.add(pads)
+
+
+	def draw_junctions(self):
+
+		junctions = JJunctions(self.dict_junctions)
+
+		self.cell.add(junctions.draw_junctions(),origin=(self.position_offs_junc_x,self.position_offs_junc_y))
+		self.cell.add(junctions.draw_junctions(),origin=(-self.position_offs_junc_x,self.position_offs_junc_y))
--- testing_dev_code.py
+++ testing_dev_code.py
@@ -1,10 +1,10 @@
 import numpy as np
 import Transmon 
 
-dict_pads = {'width': 25,
-			'height': 25,
+dict_pads = {'width': 250,
+			'height': 600,
 			'lead_width': 3,
-			'lead_height':10,
+			'lead_height':20,
 			'fork_depth': 1,
 			'rounded_edges':True,
 			'layer':1}
@@ -19,16 +19,24 @@ dict_pads = {'width': 25,
 # 			'layer':2}
 
 # adding 0.1 at the end of the equation makes sure that we have some extra room
-# 
 
+# Note that overlap cannot be negative!!!!!
 w_dolan_bridge = 0.12
 appr_overlap = (2*(700 - 45)*np.tan(35*np.pi/180) - w_dolan_bridge*1e3)/1e3 + 0.1
 dict_junctions = {'bjunction_width':2,
 				'bjunction_height':4,
-				'junction_width':0.5,
+				'junction_width':0.1,
 				'junction_height':1,
-				'w_dolan_bridge':0.12,
+				'w_dolan_bridge':w_dolan_bridge,
 				'appr_overlap':appr_overlap,
 				'layer':3}
-first_qubit = Transmon.singlejuction_transmon('test',dict_pads,
-											dict_junctions,short = False)
+
+dict_squidloop = {'squid_thickness':3,
+				'squid_width': 30,
+				'squid_height': 10}
+# first_qubit = Transmon.singlejuction_transmon('test',dict_pads,
+# 											dict_junctions,short = False)
+
+
+first_qubit = Transmon.squidjunction_transmon('test',dict_pads,dict_squidloop,
+											dict_junctions)
