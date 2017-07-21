8e482a26dccca1bcd37adf1c9df4f53e3d3ce5aa
feschmidt <felix.e.schmidt@gmail.com>
2017-07-21 18:38:08 +0200

    Update felix_test.py
    works for junction_ald and squid_ald

--- felix_test.py
+++ felix_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from source_dev.chip import Base_Chip
-import funcs.junction_ald_array as junction_array
+import source_dev.junction_ald_array as junction_array
+import source_dev.squid_ald_array as squid_array
 """
 For junctiontest layer 14,20,21 reserved
 """
@@ -9,20 +10,31 @@ dict_pads = {'width': 200,
             'spacing': 300,
             'triheight': 100}
 
-dict_junctions = {'width': 20,
+dict_junctions = {'width': 10,
                 'jjmin': 1,
                 'jjmax': 7,
                 'jjstep': 1}
 
-name = 'testjunctions'
+dict_squids = {'width': 10,
+                'jjmin': 1,
+                'jjmax': 7,
+                'jjstep': 1}
+                
 
+name = 'testjunctions'
 testf = junction_array.Junctionchip(name,dict_pads,dict_junctions, x0 = -100,
-        y0 = -2200, tlength = 1600, chipsize = 6000)
-testf.gen_junctions(marker = False, vernier = False, testpads = True)
+        y0 = -2200, tlength = 1600)
+testf.gen_junctions()
+'''
+name = 'testsquids'
+testf = squid_array.Junctionchip(name,dict_pads,dict_squids, x0 = -100,
+        y0 = -2200, tlength = 1510)
+testf.gen_junctions(dim=(50,50))
+'''
 
 chip = Base_Chip(name,6000,6000)
 chip.add_component(testf.cell,(3000,3000))
 chip.add_photolitho_marker()
 chip.add_photolitho_vernier()
-chip.save_to_gds(show = True, save =False)
-
+chip.add_bond_testpads()
+chip.save_to_gds(show = True, save = True)
