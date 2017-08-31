3df40607cca49c3eff2dd21bc82cf2b810c35242
feschmidt <felix.e.schmidt@gmail.com>
2017-08-31 08:36:31 +0200

    bugfixes

--- testing_scripts/felix_test_jpa.py
+++ testing_scripts/felix_test_jpa.py
@@ -1,10 +1,5 @@
-import numpy as np
 from source_dev.chip import Base_Chip
-from source_dev.junction_ald_array import Junctionchip
-from source_dev.squid_ald_array import SQUIDchip
-from source_dev.rfcavities_dcbias import ShuntCavity
 import source_dev.rf_feedlines as feedline
-import source_dev.rf_hangers as hangers
 import source_dev.jpa_shunt_ald as jpa_shunt
 
 
@@ -35,10 +30,23 @@ dict_jpa = {'feedlength': feedlength,
 jpa = jpa_shunt.JPA('JPA', dict_jpa)
 rf_jpa = jpa.gen_full()
 
+dict_jpa2 = {'feedlength': feedlength,
+            'position': (-4000,3489),
+            'shunt': (80,125),
+            'squid': (10,10,1,3),
+            'squidfeed': 15,
+            'centerwidth': 10,
+            'gapwidth': 6}
+
+jpa2 = jpa_shunt.JPA('JPA', dict_jpa2)
+rf_jpa2 = jpa2.gen_full()
+
 chipsize = 10e3
 chip = Base_Chip('JPA TEST', chipsize, chipsize)
 chip.add_component(rf_jpa, (0,0))
 chip.add_component(rf_feed_hor, (0,-3500))
+chip.add_component(rf_jpa2, (0,0))
+chip.add_component(rf_feed_hor, (0,3500))
 chip.add_ebpg_marker((-3300, -1500))
 chip.add_ebpg_marker((feedlength-4150, -3750), spacing=500, duplicate=False)    # add extra markers for trimming
 chip.add_photolitho_marker()
