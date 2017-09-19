556acafb5e2d84f90e6ad89da40efa81a013c958
Mario Gely - TNW <mfgely@tudelft.net>
2017-09-19 14:39:32 +0200

    updated inductor design

--- source_dev/spiral_inductor.py
+++ source_dev/spiral_inductor.py
@@ -1,106 +1,73 @@
 import numpy as np
+from stcad.source_dev.chip import Base_Chip
 import gdsCAD as cad
 import copy
 
 class SpiralInductor(cad.core.Cell):
     """docstring for SpiralInductor"""
-    def __init__(self,
-        interior = 3, 
-        exterior = 90,
-        coil_number = 40,
-        linewidth = 0.5,
-        bridge_gap = 2,
-        bridge_base_length = 3,
-        bridge_base_overlap_length = 0.1,
-        bridge_base_overlap_width = 0.05,
+    def __init__(self, 
+        exterior = 55.,
+        coil_number = 45,
+        line_width = 0.25,
+        spacing = 0.25,
+        bridge_width = 2.,
+        overlap_square_width = 3.,
+        tail_length = 5.,
         base_layer = 1,
         sacrificial_layer = 2,
-        bridge_layer = 3,
+        top_layer = 3,
         name=''):   
 
 
         super(SpiralInductor, self).__init__(name)
-        di = interior
-        self.di = di
-        do = exterior
-        self.do = do
+        do = float(exterior)
         n = coil_number
+        self.do = do
         self.n = n
-        width = linewidth
-        gap = bridge_gap
-        pitch = (do-di)/(n-1)/2
+        pitch = line_width+spacing
+        self.di = do-2.*pitch*n
 
 
         ##################
-        # Draw Spiral
+        # Spiral
         ##################
 
 
         spiral = cad.core.Elements()
         points = [[0,0]]
         for i in range(n-1):
-          points.append([i*pitch,-do/2+gap/2])
-          spiral.add(cad.core.Path(points, width,layer = base_layer))
-          points = []
-          points.append([i*pitch,-do/2-gap/2])
           points.append([i*pitch,-do+i*pitch])
           points.append([do-i*pitch,-do+i*pitch])
           points.append([do-i*pitch,-i*pitch])
           points.append([(i+1)*pitch,-i*pitch])
-        points.append([(n-1)*pitch,-do/2+gap/2])
-        spiral.add(cad.core.Path(points, width,layer = base_layer))
-        points = []
-        points.append([(n-1)*pitch,-do/2-gap/2])
         points.append([(n-1)*pitch,-do+(n-1)*pitch])
         points.append([do-(n-1)*pitch,-do+(n-1)*pitch])
-        points.append([do-(n-1)*pitch,-do/2])
-        # points.append([(n)*pitch,-(n-1)*pitch])
-        # points.append([(n)*pitch,-do/2])
-        spiral.add(cad.core.Path(points, width,layer = base_layer))
+        points.append([do-(n-1)*pitch,-do/2.])
+        points.append([do/2.,-do/2.])
+        spiral.add(cad.core.Path(points, line_width,layer = top_layer))
+
+
+      # ##################
+      # # Base layer
+      # ##################
+
+
+        overlap_square = cad.shapes.Rectangle((do/2.-overlap_square_width/2.,-do/2.-overlap_square_width/2.),\
+         (do/2.+overlap_square_width/2.,-do/2.+overlap_square_width/2.), layer = base_layer)
+        tail = cad.shapes.Rectangle((-tail_length,-do/2.+line_width),\
+         (do/2.,-do/2.-line_width), layer = base_layer)
+
+
+      # ##################
+      # # Sacrificial layer
+      # ##################
+
+        sacrificial = cad.shapes.Rectangle((-tail_length,-do/2.+bridge_width/2.),\
+         (do/2.-overlap_square_width/2.,-do/2.-bridge_width/2.), layer = sacrificial_layer)
 
 
 
-        ##################
-        # Draw tail of spiral
-        ##################
-
-
-        start_tail = copy.copy(points[-1])
-        end_tail = [0,-do/2]
-        tail = cad.core.Path([start_tail,end_tail], width,layer = base_layer)
-
-
-
-        ##################
-        # Draw base of bridge
-        ##################
-
-
-        bridge_base = cad.core.Elements()
-        for i in range(n):
-          points = []
-          points.append([i*pitch,-do/2+gap/2+bridge_base_length])
-          points.append([i*pitch,-do/2+gap/2-bridge_base_overlap_length])
-          bridge_base.add(cad.core.Path(points, width+bridge_base_overlap_width*2,layer = sacrificial_layer))
-          points = []
-          points.append([i*pitch,-do/2-gap/2+bridge_base_overlap_length])
-          points.append([i*pitch,-do/2-gap/2-bridge_base_length])
-          bridge_base.add(cad.core.Path(points, width+bridge_base_overlap_width*2,layer = sacrificial_layer))
-
-
-        ##################
-        # Draw bridge
-        ##################
-
-        bridge_metal = cad.core.Elements()
-        for i in range(n):
-          points = []
-          points.append([i*pitch,-do/2+gap/2])
-          points.append([i*pitch,-do/2-gap/2])
-          bridge_metal.add(cad.core.Path(points, width,layer = bridge_layer))
-
-
-        self.add([spiral,bridge_metal,bridge_base,tail])
+        self.add([spiral,overlap_square,tail,sacrificial])
 
     def inductance(self):
 
@@ -109,6 +76,12 @@ class SpiralInductor(cad.core.Cell):
         rho = (self.do-self.di)/(self.do+self.di)
         da = 0.5*(self.do+self.di)
         mu_0 = 4.*np.pi*1.e-7
-        return k1*mu_0*self.n**2*da*1.e-6/(1+k2*rho)
+        return k1*mu_0*float(self.n)**2*da*1.e-6/(1+k2*rho)
 
-
+if __name__ == '__main__':
+  chipsize = 200
+  chip = Base_Chip('inductor', chipsize, chipsize,label=False)
+  inductor = SpiralInductor()
+  print inductor.inductance()
+  chip.add_component(inductor, (0,0))
+  chip.save_to_gds(show=True, save=True,loc='')
--- testing_scripts/mario_inductor.py
+++ testing_scripts/mario_inductor.py
@@ -6,5 +6,6 @@ import gdsCAD as cad
 chipsize = 200
 chip = Base_Chip('inductor', chipsize, chipsize,label=False)
 inductor = SpiralInductor()
+print inductor.inductance()
 chip.add_component(inductor, (0,0))
 chip.save_to_gds(show=True, save=True,loc='')
