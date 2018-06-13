9eb5594450811321e4ec56ee56117078b2dd4f70
feschmidt <felix.e.schmidt@gmail.com>
2018-06-13 15:52:37 +0200

    reduced size of small cells

--- source_dev/groundplaneholes.py
+++ source_dev/groundplaneholes.py
@@ -13,8 +13,8 @@ class GroundPlaneHoles():
 
         self.xdim = 10e3
         self.ydim = 10e3
-        self.smallx = 1e3
-        self.smally = 1e3
+        self.smallx = 1e2
+        self.smally = 1e2
         self.holes = [2.,2.]
         self.streets = [1.,1.]
 
@@ -27,7 +27,7 @@ class GroundPlaneHoles():
     def gen_full(self):
         """
         """
-        self.smallcell = cad.core.Cell('HOLES')
+        self.smallcell = cad.core.Cell('SMALL HOLES')
 
         xlist = np.arange(-self.smallx/2.,self.smallx/2.,self.holes[0]+self.streets[0])
         ylist = np.arange(-self.smally/2.,self.smally/2.,self.holes[1]+self.streets[1])
@@ -39,14 +39,16 @@ class GroundPlaneHoles():
                 hole = cad.shapes.Rectangle((x,y),(x+self.holes[0],y+self.holes[1]),layer=self.layer)
                 self.smallcell.add(hole)
                 k += 1.
-                print((x,y), '{:.4} %'.format(k/len(xlist)/len(ylist)*100.))
+                print('smallcell',(x,y), '{:.4} %'.format(k/len(xlist)/len(ylist)*100.))
 
-        smallxlist = np.arange(-self.xdim/2.,self.xdim/2.,self.smallx+2*self.streets[0])
-        smallylist = np.arange(-self.ydim/2.,self.ydim/2.,self.smally+2*self.streets[0])
+        smallxlist = np.arange(-self.xdim/2.,self.xdim/2.,int(self.smallx+2*self.streets[0]))
+        smallylist = np.arange(-self.ydim/2.,self.ydim/2.,int(self.smally+2*self.streets[0]))
+ 
         k = 0
         for i,x in enumerate(smallxlist):
             for j,y in enumerate(smallylist):
                 self.cell.add(cad.core.CellReference(self.smallcell,origin=(x+self.smallx/2.,y+self.smally/2.)))
-                print((x,y), '{:.4} %'.format(k/len(xlist)/len(ylist)*100.))
+                print('bigcell',(x,y), '{:.4} %'.format(k/len(smallxlist)/len(smallylist)*100.))
+                k+=1
         return self.cell
 
